d0dab7accf1e7af726d6309cff54e940e185495f
Irza Hasan <irzahasan07@gmail.com>
2025-05-24 22:00:28 +0500

    Execution file added

---
+++ src/consulnet/Execution.java
@@ -0,0 +1,10 @@
+package consulnet;
+
+public class Execution {
+    public static void main(String[] args) {
+
+//        Calculator calculator = new Calculator();
+//        calculator.calculatorAction(3,7,'-');
+
+    }
+}
